9d064f06f4fcb7d666a3c0ffa90b29929e868916
Kevin <stupark@bzz.ch>
2022-01-12 12:10:27 +0100

    Sort Interface set, Data Class finished to use in Sort algorythms and to use in Managementsystems. Data Class modified

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,29 @@
 public class Data {
+    private static final int QUICKSORT_FIRST = 0;
+    private static final int QUICKSORT_RANDOM = 1;
+    private static final int SHAKERSORT = 2;
+    private static final int BINARYTREESORT = 3;
+    private static final int INSERTIONSORT = 4;
+    private static final int MERGESORT = 5;
+    private int sortType;
     private int countWrite;
     private int countCompare;
     private long nanoTime;
     private int storageSpace;
     private int[] array;
+
+    public Data(int sortType) {
+        this.sortType = sortType;
+    }
+
+    public int getSortType() {
+        return sortType;
+    }
+
+    public void setSortType(int sortType) {
+        this.sortType = sortType;
+    }
+
 
     public int getCountWrite() {
         return countWrite;
