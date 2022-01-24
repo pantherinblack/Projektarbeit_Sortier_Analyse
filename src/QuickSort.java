1eb327ea60c5cb867d4b49e0aba38bca7ab57cc5
moustafahawi <hawim@bzz.ch>
2022-01-24 19:09:58 +0100

    Kevin Test my Quicksort please!

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 
 /**
  * Diese Klasse sortiert ein Array nach einem beliebigem Index der am Anfang
@@ -51,7 +52,9 @@ public class QuickSort implements SortierIF{
              * Tausche Elemente:
              * 1. zwischenresultat hält den Wert von der linken Seite fest.
              * 2. Dann wird der Wert an der Position der linken Seite auf der Wert der rechten Seite geändert.
-             * 3.
+             * 3. Dann wird der wer an der Position der rechten Seite auf den Wert zwischenresultat geändert.
+             * 4. Auf der linken Seite um eins weitergeleitet (positionleft ++)
+             * 5. Auf der rechten Seite um eins zurückgehen (positionright --)
              */
             if (positionleft <= positionright){
                 int zwischenResultat = array[positionleft];
@@ -61,8 +64,37 @@ public class QuickSort implements SortierIF{
                 positionright--;
             }
         }
-        return 0;
+        return positionleft;
     }
+
+    /**
+     * Diese Methode ruft die Methode split() so lange auf bis es das Array durchsortiert hat.
+     * @param array
+     * @param first
+     * @param last
+     */
+    public void sort(int[] array, int first, int last){
+        /**
+         * Aufteilung des Arrays in die zwei Seiten. Die linke Seit wird kleiner als Pivot und rechte Seite grösser als Pivot.
+         */
+        int index = split(array, first, last);
+        /**
+         * Das repräsentiert die linke Seite vom Pivot-Element.
+         */
+        if (first < index - 1){
+            sort(array, first, index - 1);
+        }
+        /**
+         * Das repräsentiert die rechte Seite vom Pivot-Element.
+         */
+        if (index < last){
+            sort(array, index, last);
+        }
+    }
+    /**
+     *  Diese Methode ruft die Methode sort() (die mit den Parametern array : int[], first : int, last : int.
+     * @param array
+     */
     @Override
     public void sort(int[] array) {
 
@@ -70,6 +102,20 @@ public class QuickSort implements SortierIF{
 
     @Override
     public Data getData() {
+        //todo: return data
         return null;
+    }
+
+    public static void main(String[] args) {
+        QuickSort q = new QuickSort();
+        int [] beispielArray = {6,23,534,12,4,1,65,3};
+        for (int i = 0; i < beispielArray.length; i++){
+            System.out.println(beispielArray[i]);
+        }
+        q.sort(beispielArray, 0, beispielArray.length);
+        System.out.println("-------------------------");
+        for (int i = 0; i < beispielArray.length; i++){
+            System.out.println(beispielArray[i]);
+        }
     }
 }
