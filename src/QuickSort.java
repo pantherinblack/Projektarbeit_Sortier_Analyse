d72e6989013cbc5b206e8031b63a9d5a2ad49115
moustafahawi <hawim@bzz.ch>
2022-01-24 16:58:46 +0100

    QuickSort class by Moustafa, not finished yet.

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,27 +1,70 @@
 
 /**
+ * Diese Klasse sortiert ein Array nach einem beliebigem Index der am Anfang
+ * initialisiert wird.
  * @since 13.01.2022
  * @author Moustafa Hawi
  * @version 0.1
- * This class sort a class with a quick Sort.
  */
 
 public class QuickSort implements SortierIF{
-    @Override
-    public void sort(int[] array) {
-
-    }
-
     /**
      *
-     * @param array This is the array that we will sort.
-     * @param left This are the elements that are smaller than the pivot.
-     * @param right This are the elements that are bigger than the pivot.
-     * That method will take the array and sort it with a pivot that is the middle index of the array. All values before
-     * the pivot are smaller than the pivot and all values that comes after the array are bigger.
+     * @param array
+     * @param first
+     * @param last
+     * @return
      */
-    public void sort(int[] array, int left, int right){
-        int pivot = (int) (Math.random() * (right - left) + left);
+    public int split(int[] array, int first, int last){
+        /**
+         * Es wird aus dem Array ein beliebiger index genommen und in der variable pivot gespeichert.
+         */
+        //todo random index
+        int pivot = array[(first + last)/2];
+        /**
+         * Zeigt auf das erste Element der Liste.
+         * Wird auch gegebenenfalls später inkrementiert.
+         */
+        int positionleft = first;
+        /**
+         * Zeigt auf das letzte Element der Liste.
+         * Wird auch gegebenenfalls später dekrementiert.
+         */
+        int positionright = last;
+
+        while (positionleft <= positionright){
+            /**
+             * Solange die Elemente auf der linken Seite kleiner als das pivot Element
+             * sind, erhöht die es die Position.
+             */
+            while (array[positionleft] < pivot){
+                positionleft++;
+            }
+            /**
+             * Solange die Elemente auf der rechten Seite grösser als das Pivot-Element
+             * sind, verringet es die Position.
+             */
+            while (array[positionright] > pivot){
+                positionright--;
+            }
+            /**
+             * Tausche Elemente:
+             * 1. zwischenresultat hält den Wert von der linken Seite fest.
+             * 2. Dann wird der Wert an der Position der linken Seite auf der Wert der rechten Seite geändert.
+             * 3.
+             */
+            if (positionleft <= positionright){
+                int zwischenResultat = array[positionleft];
+                array[positionleft] = array[positionright];
+                array[positionright] = zwischenResultat;
+                positionleft++;
+                positionright--;
+            }
+        }
+        return 0;
+    }
+    @Override
+    public void sort(int[] array) {
 
     }
 
