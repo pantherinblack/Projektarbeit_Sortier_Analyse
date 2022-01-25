388a64a6ef1242dc291d776f2b9a3ae506274fe4
moustafahawi <hawim@bzz.ch>
2022-01-25 19:16:38 +0100

    CocktailSort is done :)

--- src/CocktailSort.java
+++ src/CocktailSort.java
@@ -1,21 +1,34 @@
 
 /**
- * Diese Klasse sortiert ein Array nach einem beliebigem Index der am Anfang
- * initialisiert wird.
+ * This class sorts a array with a CocktailSort and returns the data.
  * @since 25.01.2022
  * @author Moustafa Hawi
  * @version 0.1
  */
 
 public class CocktailSort implements SortIF{
+    private int countBits = 0;
+    private long nanoTime = 0;
+    private int[] a;
+    private int countSwaps = 0;
+    private int countCompares = 0;
 
+    /**
+     * This Method sorts the Array.
+     * @param array
+     */
     @Override
     public void sort(int[] array) {
+        nanoTime = - System.nanoTime();
         boolean swapped = true;
+        countBits++;
         int start = 0;
+        countBits++;
         int end = array.length;
 
+        countCompares++;
         while (swapped == true){
+            countCompares++;
             /**
              * Reset the swapped flag on entering the loop, because it might be true from a previous iteration.
              */
@@ -24,8 +37,13 @@ public class CocktailSort implements SortIF{
         /**
          * loop from bottom to top same as the bubble sort.
          */
+        countCompares++;
         for (int i = start; i < end - 1; i++){
+            countCompares++;
+            countCompares++;
             if (array[i] > array[i + 1]){
+                countBits++;
+                countSwaps+=2;
                 int temporär = array[i];
                 array[i] = array[i + 1];
                 array[i + 1] = temporär;
@@ -35,6 +53,7 @@ public class CocktailSort implements SortIF{
         /**
          * If nothing moved, then array is sorted.
          */
+        countCompares++;
         if (swapped == false) {
             return;
         }
@@ -50,8 +69,13 @@ public class CocktailSort implements SortIF{
         /**
          * From top to bottom, doing the same comparison as in the previous stage.
          */
+            countCompares++;
             for (int i = end - 1; i >= start; i--){
+                countCompares++;
+                countCompares++;
                 if (array[i] > array[i + 1]){
+                    countBits++;
+                    countSwaps+=2;
                     int temporär = array[i];
                     array[i] = array[i + 1];
                     array[i + 1] = temporär;
@@ -62,11 +86,22 @@ public class CocktailSort implements SortIF{
              * increase the starting point, because the last stage would have moved the next smallest number to its rightful spot.
              */
             start = start + 1;
+            nanoTime += System.nanoTime();
+            a = array.clone();
         }
 
-
+    /**
+     * This Method return data.
+     * @return data
+     */
     @Override
     public Data getData() {
-        return null;
+        Data data = new Data("CocktailSort");
+        data.setStorageSpace(countBits*32);
+        data.setNanoTime(nanoTime);
+        data.setArray(a);
+        data.setCountWrite(countSwaps);
+        data.setCountCompare(countCompares);
+        return data;
     }
 }
