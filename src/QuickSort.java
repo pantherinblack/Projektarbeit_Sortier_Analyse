903c944c690a82a2574898d650a70436cb1763e5
moustafahawi <hawim@bzz.ch>
2022-01-25 22:19:28 +0100

    Comments Quicksort fixed

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -2,8 +2,10 @@ import java.lang.reflect.Array;
 import java.util.Random;
 
 /**
- * Diese Klasse sortiert ein Array nach einem beliebigem Index der am Anfang
- * initialisiert wird.
+ * Like Merge Sort, QuickSort is a Divide and Conquer algorithm.
+ * It picks an element as pivot and partitions the given array around the picked pivot.
+ * There are many different versions of quickSort that pick pivot in different ways.
+ * This one picks the first element as pivot.
  * @since 13.01.2022
  * @author Moustafa Hawi
  * @version 0.1
@@ -16,7 +18,11 @@ public class QuickSort implements SortIF {
     private long countSwaps = 0;
     private long countCompares = 0;
     /**
-     *
+     * This function takes first element as pivot, places
+     * the pivot element at its correct position in sorted
+     * array, and places all smaller (smaller than pivot)
+     * to left of pivot and all greater elements to right
+     * of pivot
      * @param array
      * @param first
      * @param last
@@ -24,20 +30,19 @@ public class QuickSort implements SortIF {
      */
     public int split(int[] array, int first, int last){
         /**
-         * Es wird aus dem Array ein beliebiger index genommen und in der variable pivot gespeichert.
+         * takes first element as pivot
          */
-        //todo random index
         countBits++;
         int pivot = first;
         /**
-         * Zeigt auf das erste Element der Liste.
-         * Wird auch gegebenenfalls später inkrementiert.
+         * Shows the first element of the list.
+         * will be increased later
          */
         countBits++;
         int positionleft = first;
         /**
-         * Zeigt auf das letzte Element der Liste.
-         * Wird auch gegebenenfalls später dekrementiert.
+         * shows the last element of the list.
+         * will be reduced later
          */
         countBits++;
         int positionright = last;
@@ -45,8 +50,8 @@ public class QuickSort implements SortIF {
         while (positionleft <= positionright){
             countCompares++;
             /**
-             * Solange die Elemente auf der linken Seite kleiner als das pivot Element
-             * sind, erhöht die es die Position.
+             * As long as the elements on the left side are smaller than the pivot element
+             * are, it increases the position.
              */
             countCompares++;
             while (array[positionleft] < array[pivot]){
@@ -54,8 +59,8 @@ public class QuickSort implements SortIF {
                 countCompares++;
             }
             /**
-             * Solange die Elemente auf der rechten Seite grösser als das Pivot-Element
-             * sind, verringet es die Position.
+             * As long as the elements on the right side are bigger than the pivot element
+             * it reduces the position.
              */
             countCompares++;
             while (array[positionright] > array[pivot]){
@@ -63,12 +68,12 @@ public class QuickSort implements SortIF {
                 countCompares++;
             }
             /**
-             * Tausche Elemente:
-             * 1. zwischenresultat hält den Wert von der linken Seite fest.
-             * 2. Dann wird der Wert an der Position der linken Seite auf der Wert der rechten Seite geändert.
-             * 3. Dann wird der wer an der Position der rechten Seite auf den Wert zwischenresultat geändert.
-             * 4. Auf der linken Seite um eins weitergeleitet (positionleft ++)
-             * 5. Auf der rechten Seite um eins zurückgehen (positionright --)
+             * Exchange elements:
+             * 1. intermediate result holds the value from the left side.
+             * 2. Then the value at the position of the left side is changed to the value of the right side.
+             * 3. Then the who at the position of the right side is changed to the value betweenresult.
+             * 4. On the left side forwarded by one (positionleft ++)
+             * 5. Go back one on the right side (positionright --)
              */
             countCompares++;
             if (positionleft <= positionright){
@@ -85,26 +90,26 @@ public class QuickSort implements SortIF {
     }
 
     /**
-     * Diese Methode ruft die Methode split() so lange auf bis es das Array durchsortiert hat.
+     *  This method calls the split() method until it has sorted through the array.
      * @param array
      * @param first
      * @param last
      */
     public void sort(int[] array, int first, int last){
         /**
-         * Aufteilung des Arrays in die zwei Seiten. Die linke Seit wird kleiner als Pivot und rechte Seite grösser als Pivot.
+         * Splitting the array into the two sides. The left side becomes smaller than Pivot and the right side becomes larger than Pivot.
          */
         countBits++;
         int index = split(array, first, last);
         /**
-         * Das repräsentiert die linke Seite vom Pivot-Element.
+         * This represents the left side of the pivot element.
          */
         countCompares++;
         if (first < index-1){
             sort(array, first, index - 1);
         }
         /**
-         * Das repräsentiert die rechte Seite vom Pivot-Element.
+         * This represents the right side of the pivot element.
          */
         countCompares++;
         if (index < last){
@@ -112,7 +117,7 @@ public class QuickSort implements SortIF {
         }
     }
     /**
-     *  Diese Methode ruft die Methode sort() (die mit den Parametern array : int[], first : int, last : int.
+     * This method calls the method sort() (which is called with the parameters array : int[], first : int, last : int.
      * @param array
      */
     @Override
@@ -123,6 +128,10 @@ public class QuickSort implements SortIF {
         a = array.clone();
     }
 
+    /**
+     * This method returns data
+     * @return data
+     */
     @Override
     public Data getData() {
         Data data = new Data("QuickSort-random");
